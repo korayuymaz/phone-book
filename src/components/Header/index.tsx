855caeae6c04e7ed04ae09e58d6176900cc23aae
Koray <koray.uymaz@agesa.com.tr>
2024-10-26 21:31:45 +0300

    Header Component and pages structures added

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import './index.scss'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-logo'>Logo</div>
+      <div className='header-links'>
+        <div>Numbers</div>
+        <div>Login</div>
+        <div>Sign In</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
